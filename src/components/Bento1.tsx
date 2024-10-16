d8bde18aba93bc3c0dc7f73c3c9f9a759cd26bea
Anish <xevenbiswas@gmail.com>
2024-10-16 20:39:46 +0530

    integrate into one

--- src/components/Bento1.tsx
+++ src/components/Bento1.tsx
@@ -3,12 +3,25 @@ import { useEffect, useState } from "react";
 
 import { Space_Grotesk } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { Clipboard, Linkedin, TwitterIcon } from "lucide-react";
+import {
+  Activity,
+  Calendar,
+  Clipboard,
+  Flame,
+  GitBranch,
+  GitPullRequest,
+  Linkedin,
+  Star,
+  Trophy,
+  TwitterIcon,
+  Users,
+} from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import GitHubStatsCard from "./GithubStatCard";
 import { StreakStats, UserStats } from "@/types";
 import GitHubStreakCard from "./GithubStreakCard";
+import Image from "next/image";
 
 const space = Space_Grotesk({
   subsets: ["latin"],
@@ -146,10 +159,219 @@ const BentoGrid = ({
         {stats && showStats && (
           <div className="grid gap-4 grid-cols-4 col-span-4 row-span-2">
             <div className="col-span-2 row-span-2">
-              <GitHubStatsCard stats={stats} userName={githubURL} />
+              <div className="grid grid-cols-4 grid-rows-3 gap-4 auto-rows-fr rounded-xl overflow-hidden size-full">
+                <div className="bg-gradient-to-br from-amber-500/40 via-amber-500/10 to-transparent rounded-xl p-4 flex flex-col justify-between col-span-2 relative row-span-2">
+                  <div className="flex absolute top-2 px-3 left-0 items-center justify-between w-full opacity-70">
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                  </div>
+                  <h3 className="text-2xl mt-16 text-end text-muted-foreground font-medium">
+                    Total Stars
+                  </h3>
+                  <div className="text-end text-yellow-400 text-7xl font-bold">
+                    {stats["Star Earned"]}
+                  </div>
+                </div>
+
+                <div className="bg-gradient-to-b from-pink-900/20 to-neutral-900/50 rounded-xl relative p-4 flex flex-col justify-between col-span-1 row-span-1">
+                  <GitPullRequest
+                    className="text-pink-400 absolute top-2"
+                    size={20}
+                  />
+                  <span className="text-gray-300 text-sm pt-4 font-medium">
+                    PRs
+                  </span>
+
+                  <div className="text-pink-400 text-3xl font-bold mt-2">
+                    {stats["Pull Requests"]}
+                  </div>
+                </div>
+                <div className="bg-gradient-to-tl from-rose-950/20 to-stone-900/50 relative rounded-xl p-4 flex flex-col justify-between col-span-1 row-span-1">
+                  <Users className="text-red-500 absolute top-2" size={20} />
+                  <span className="text-gray-300 text-sm pt-4 font-medium">
+                    Followers
+                  </span>
+
+                  <div className="text-red-500 text-4xl font-bold mt-2">
+                    {stats.Followers}
+                  </div>
+                </div>
+                <div className="bg-gradient-to-t from-black to-slate-800/50 overflow-hidden relative rounded-xl p-4 flex flex-col justify-between col-span-2 row-span-2">
+                  <Image
+                    src={"/surf.svg"}
+                    alt=""
+                    width={450}
+                    height={400}
+                    className="absolute inset-0 object-cover rotate-180"
+                  />
+                  <div className="flex items-center w-full">
+                    <Activity className="text-green-400" size={45} />
+                    <span className="text-muted-foreground w-full text-end text-2xl font-medium">
+                      Commits
+                    </span>
+                  </div>
+                  <div className="text-green-400 text-6xl text-end font-bold ">
+                    {stats.Commits}
+                  </div>
+                </div>
+                <div className="bg-muted/30 relative overflow-hidden rounded-xl p-4 flex flex-col justify-between col-span-2 row-span-1">
+                  <Image
+                    src={"/blocks.svg"}
+                    alt=""
+                    width={400}
+                    height={400}
+                    className="absolute -z-10 inset-0 object-cover brightness-150"
+                  />
+                  <GitBranch
+                    className="text-blue-400 glow absolute left-12 bottom-4"
+                    size={40}
+                  />
+                  <span className="text-muted-foreground text-center w-full text-sm font-medium">
+                    Contributed To
+                  </span>
+                  <div className="text-blue-400 text-4xl text-center font-bold mt-2">
+                    {stats["Contributed To"]}
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-4 grid-rows-3 gap-4 auto-rows-fr rounded-xl overflow-hidden size-full">
+                <div className="bg-gradient-to-br from-amber-500/40 via-amber-500/10 to-transparent rounded-xl p-4 flex flex-col justify-between col-span-2 relative row-span-2">
+                  <div className="flex absolute top-2 px-3 left-0 items-center justify-between w-full opacity-70">
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                    <Star size={40} fill="gold" color="gold" />
+                  </div>
+                  <h3 className="text-2xl mt-16 text-end text-muted-foreground font-medium">
+                    Total Stars
+                  </h3>
+                  <div className="text-end text-yellow-400 text-7xl font-bold">
+                    {stats["Star Earned"]}
+                  </div>
+                </div>
+
+                <div className="bg-gradient-to-b from-pink-900/20 to-neutral-900/50 rounded-xl relative p-4 flex flex-col justify-between col-span-1 row-span-1">
+                  <GitPullRequest
+                    className="text-pink-400 absolute top-2"
+                    size={20}
+                  />
+                  <span className="text-gray-300 text-sm pt-4 font-medium">
+                    PRs
+                  </span>
+
+                  <div className="text-pink-400 text-3xl font-bold mt-2">
+                    {stats["Pull Requests"]}
+                  </div>
+                </div>
+                <div className="bg-gradient-to-tl from-rose-950/20 to-stone-900/50 relative rounded-xl p-4 flex flex-col justify-between col-span-1 row-span-1">
+                  <Users className="text-red-500 absolute top-2" size={20} />
+                  <span className="text-gray-300 text-sm pt-4 font-medium">
+                    Followers
+                  </span>
+
+                  <div className="text-red-500 text-4xl font-bold mt-2">
+                    {stats.Followers}
+                  </div>
+                </div>
+                <div className="bg-gradient-to-t from-black to-slate-800/50 overflow-hidden relative rounded-xl p-4 flex flex-col justify-between col-span-2 row-span-2">
+                  <Image
+                    src={"/surf.svg"}
+                    alt=""
+                    width={450}
+                    height={400}
+                    className="absolute inset-0 object-cover rotate-180"
+                  />
+                  <div className="flex items-center w-full">
+                    <Activity className="text-green-400" size={45} />
+                    <span className="text-muted-foreground w-full text-end text-2xl font-medium">
+                      Commits
+                    </span>
+                  </div>
+                  <div className="text-green-400 text-6xl text-end font-bold ">
+                    {stats.Commits}
+                  </div>
+                </div>
+                <div className="bg-muted/30 relative overflow-hidden rounded-xl p-4 flex flex-col justify-between col-span-2 row-span-1">
+                  <Image
+                    src={"/blocks.svg"}
+                    alt=""
+                    width={400}
+                    height={400}
+                    className="absolute -z-10 inset-0 object-cover brightness-150"
+                  />
+                  <GitBranch
+                    className="text-blue-400 glow absolute left-12 bottom-4"
+                    size={40}
+                  />
+                  <span className="text-muted-foreground text-center w-full text-sm font-medium">
+                    Contributed To
+                  </span>
+                  <div className="text-blue-400 text-4xl text-center font-bold mt-2">
+                    {stats["Contributed To"]}
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="size-full col-span-2 row-span-2">
-              <GitHubStreakCard streak={streak} />
+              <div className="max-w-xl size-full rounded-xl">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 size-full">
+                  <div className="flex size-full flex-col space-y-4">
+                    <div className="bg-secondary/80 rounded-lg p-4 h-full flex flex-col items-center justify-center">
+                      <Calendar className="w-8 h-8 mb-2 text-blue-400" />
+                      <h3 className="text-sm font-medium text-gray-400">
+                        Total Contributions
+                      </h3>
+                      <p className="text-3xl font-bold text-blue-400">
+                        {streak?.totalContributions}
+                      </p>
+                      <p className="text-xs text-gray-500 mt-2">
+                        {streak?.firstDateofContribution
+                          ? streak?.firstDateofContribution + "- Present"
+                          : "No contributions yet"}
+                      </p>
+                    </div>
+                    <div className="rounded-lg bg-secondary/80 p-4 h-full flex flex-col items-center justify-center">
+                      <Trophy className="w-8 h-8 mb-2 text-yellow-400" />
+                      <h3 className="text-sm font-medium text-gray-400">
+                        Longest Streak
+                      </h3>
+                      <p className="text-3xl font-bold text-yellow-400">
+                        {streak?.longestStreak}
+                      </p>
+                      <p className="text-xs text-gray-500 mt-2">
+                        {streak?.longestStreakStartDate &&
+                        streak?.longestStreakEndDate
+                          ? streak?.longestStreakStartDate +
+                            " - " +
+                            streak?.longestStreakEndDate
+                          : "No streak yet"}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="bg-gradient-to-r to-orange-800/10 from-orange-800/10 via-muted/10 rounded-lg p-6 flex flex-col items-center justify-center relative">
+                    <Flame className="w-28 h-28 mb-4 text-orange-600 glow-orange rounded-full p-4" />
+                    <h3 className="text-lg font-medium text-gray-400">
+                      Current Streak
+                    </h3>
+                    <p className="text-6xl font-bold text-orange-600 my-4">
+                      {streak?.currentStreak}
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      {streak?.currentStreakStartDate &&
+                      streak?.currentStreakEndDate
+                        ? streak?.currentStreakStartDate +
+                          " - " +
+                          streak?.currentStreakEndDate
+                        : new Date().toLocaleDateString()}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         )}
