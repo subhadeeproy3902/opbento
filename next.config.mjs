b2001c831f27d663343627e0eb6975a6c24b4dda
subhadeeproy3902 <subha9.5roy350@gmail.com>
2024-10-24 17:17:56 +0530

    recent be it past

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.map$/,
+      use: 'raw-loader',
+      type: 'asset/resource'
+    });
+    return config;
+  }
+};
 
 export default nextConfig;
