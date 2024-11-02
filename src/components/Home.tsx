13af31de60550e29d4e10291fa901b371e1f6b7f
Anish <xevenbiswas@gmail.com>
2024-11-02 15:32:03 +0530

    clearing up

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -93,16 +93,3 @@ const HeaderBlock = () => (
     </p>
   </Block>
 );
-
-export const Footer = () => {
-  return (
-    <footer className="mt-12 mb-4">
-      <p className="text-center text-muted-foreground">
-        Made with ❤️ by{" "}
-        <a href="#" className="text-red-300 hover:underline">
-          @RealDevs
-        </a>
-      </p>
-    </footer>
-  );
-};
